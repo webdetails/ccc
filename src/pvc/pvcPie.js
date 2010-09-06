4e8094f05a78a87dd655ea8c46d8dacf4b7b8fda
pmalves <pmgalves@gmail.com>
2010-09-06 18:46:08 +0100

    - piechart nearly finished

--- src/pvc/pvcPie.js
+++ src/pvc/pvcPie.js
@@ -13,8 +13,9 @@ pvc.PieChart = pvc.Base.extend({
     this.base();
 
     var _defaults = {
-      showInnerValue: false,
-      innerGap: 0.8
+      showValues: true,
+      innerGap: 0.9,
+      explodedSliceRadius: 0
     };
 
 
@@ -33,7 +34,9 @@ pvc.PieChart = pvc.Base.extend({
 
 
     this.pieChartPanel = new pvc.PieChartPanel(this, {
-      innerGap: this.options.innerGap
+      innerGap: this.options.innerGap,
+      explodedSliceRadius: this.options.explodedSliceRadius,
+      showValues: this.options.showValues
     });
 
     this.pieChartPanel.appendTo(this.basePanel); // Add it
@@ -45,14 +48,17 @@ pvc.PieChart = pvc.Base.extend({
 
 
 /*
-   * Legend panel. Generates the legend. Specific options are:
-   * <i>legend</i> - text. Default: false
-   * <i>legendPosition</i> - top / bottom / left / right. Default: bottom
-   * <i>legendSize</i> - The size of the legend in pixels. Default: 25
+   * Pie chart panel. Generates a pie chart. Specific options are:
+   * <i>showValues</i> - Show or hide slice value. Default: false
+   * <i>explodedSliceRadius</i> - If one wants a pie with an exploded effect,
+   *  specify a value in pixels here. Default: 0
+   * <i>innerGap</i> - The size of the legend in pixels. Default: 25
    *
    * Has the following protovis extension points:
    *
-   * <i>legend_</i> - for the main legend Panel
+   * <i>chart_</i> - for the main chart Panel
+   * <i>pie_</i> - for the main pie wedge
+   * <i>pieLabel_</i> - for the main pie label
    */
 
 
@@ -61,8 +67,11 @@ pvc.PieChartPanel = pvc.BasePanel.extend({
   _parent: null,
   pvPie: null,
   pvPieLabel: null,
-  
-  innerGap: 0.8,
+  data: null,
+
+  innerGap: 0.9,
+  explodedSliceRadius: 0,
+  showValues: true,
 
 
   constructor: function(chart, options){
@@ -73,7 +82,7 @@ pvc.PieChartPanel = pvc.BasePanel.extend({
 
   create: function(){
 
-
+    var myself=this;
     this.width = this._parent.width;
     this.height = this._parent.height;
 
@@ -91,28 +100,35 @@ pvc.PieChartPanel = pvc.BasePanel.extend({
     pvc.log("Radius: "+ r + "; Maximum sum: " + sum);
 
     var a = pv.Scale.linear(0, sum).range(0, 2 * Math.PI);
+    this.data = this.chart.dataEngine.getValuesForSeriesIdx(0);
+
 
     this.pvPie = this.pvPanel.add(pv.Wedge)
     .data(this.chart.dataEngine.getValuesForSeriesIdx(0))
-    .bottom(this.height / 2)
-    .left(this.width / 2)
-    .strokeStyle("white")
+    .bottom(function(d){
+      return myself.explodeSlice("cos",myself.explodedSliceRadius, a, this.index);
+    })
+    .left(function(d){
+      return myself.explodeSlice("sin", myself.explodedSliceRadius, a, this.index);
+    })
     .outerRadius(r)
     .angle(a)
     .cursor("pointer")
     .event("click",function(d){
-      pvc.log("You clicked on index " + this.index + ", value " + d)
+      pvc.log("You clicked on index " + this.index + ", value " + d + ", angle: " + myself.accumulateAngle(a,this.index));
     });
 
     // Extend pie
     this.extend(this.pvPie,"pie_");
 
 
-    this.pvPieLabel = this.pvPie.anchor("center").add(pv.Label)
-    .textAngle(0)
+    this.pvPieLabel = this.pvPie.anchor("outer").add(pv.Label)
+    //.textAngle(0)
     .text(function(d){
-      return d.toFixed(2)
-    });
+      return " "+ d.toFixed(2)
+    })
+    .textMargin(10)
+    .visible(this.showValues);
 
     // Extend pieLabel
     this.extend(this.pvPieLabel,"pieLabel_");
@@ -121,6 +137,21 @@ pvc.PieChartPanel = pvc.BasePanel.extend({
     // Extend body
     this.extend(this.pvPanel,"chart_");
 
+
+  },
+
+  accumulateAngle: function(a,idx){
+
+    var arr = this.data.slice(0,idx);
+    arr.push(this.data[idx]/2);
+    var angle = a(pv.sum(arr));
+    return angle;
+
+  },
+
+  explodeSlice: function(fun, size, a, idx){
+
+    return (fun=="cos"?this.height:this.width)/2 + size*Math[fun](this.accumulateAngle(a,idx));
 
   }
 
