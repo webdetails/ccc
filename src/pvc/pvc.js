00e007573babd72527bb04e3d6ce788b7b711c1a
pmalves <pmgalves@gmail.com>
2011-01-11 16:45:31 +0000

    - Changed verification of svg support

--- src/pvc/pvc.js
+++ src/pvc/pvc.js
@@ -83,5 +83,5 @@ if (!Array.prototype.filter)
  *
  **/
 (function($){
-    $.support.svg = $.support.svg || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.0");    
+    $.support.svg = $.support.svg || document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#BasicStructure", "1.1");
 })(jQuery);
