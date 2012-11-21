b6c7bfae169d894f0c50bb4e1043d349f448bf13
Duarte Cunha Leao <duarte.leao@gmail.com>
2012-11-21 19:27:05 +0000

    [WIP] Added the jsdoc-toolkit to the solution [WIP] Regenerated documentation

--- generateDocs.sh
+++ generateDocs.sh
@@ -13,5 +13,5 @@ java  -jar doc/lib/saxon9he.jar -xsl:doc/gen/summary/com2html-summary.xsl -s:doc
 # Invoke XSLT
 java  -jar doc/lib/saxon9he.jar -xsl:doc/gen/com2jsdoc.xsl -s:doc/model/pvc.options.xml -o:doc/model/pvc.options.xml.js relativePath=../model/
 
-JSDOCDIR="../../tools/jsdoc_toolkit-2.4.0/jsdoc-toolkit/"
+JSDOCDIR="build/jsdoc_toolkit-2.4.0/"
 java -jar ${JSDOCDIR}jsrun.jar ${JSDOCDIR}app/run.js --private --verbose --recurse doc/model/ -t=${JSDOCDIR}templates/jsdoc -d=doc/jsdoc
