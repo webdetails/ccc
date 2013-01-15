141110c7dd52678e026d9b3694a7600b56e111c7
Duarte Cunha Leao <duarte.leao@gmail.com>
2013-01-15 14:42:59 +0000

    [FIX](AxisPanel labelSpacingMin) Tuned the default value to be less aggressive

--- src/pvc/pvcAxisPanel.js
+++ src/pvc/pvcAxisPanel.js
@@ -25,7 +25,7 @@ def
     
     if(this.labelSpacingMin == null){
         // The user tolerance for "missing" stuff is much smaller with discrete stuff
-        this.labelSpacingMin = this.isDiscrete ? 0.3 : 1.5; // em
+        this.labelSpacingMin = this.isDiscrete ? 0.1 : 1.5; // em
     }
     
     if(this.showTicks == null){
