12a6daeeab2ae03f6186e7771768677bc33048a1
Duarte Cunha Leao <duarte.leao@gmail.com>
2013-04-01 15:12:19 +0100

    [WIP](treemap headers) Missing file

---
+++ src/pvc/visual/TreemapDiscreteByParentColorAxis.js
@@ -0,0 +1,16 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/*global pvc_Size:true, pvc_Axis:true */
+
+def
+.type('pvc.visual.TreemapDiscreteByParentColorAxis', pvc.visual.ColorAxis)
+.add(/** @lends pvc.visual.TreemapDiscreteByParentColorAxis# */{
+    _getBaseScheme: function() { 
+        var baseScheme = this.option('Colors');
+        
+        
+        return ;
+    }
+});
