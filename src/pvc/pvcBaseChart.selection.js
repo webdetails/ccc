b02c4d1c3c4d05f5b049e58a415087c5c75bbfb6
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-05-30 00:18:15 +0100

    [FIX](userSelectionAction) Documentation stated that the handler supplied a pvc.visual.Context as JS content/this but it was being called on the global object.

--- src/pvc/pvcBaseChart.selection.js
+++ src/pvc/pvcBaseChart.selection.js
@@ -6,31 +6,31 @@ pvc.BaseChart
 .add({
     _updateSelectionSuspendCount: 0,
     _lastSelectedDatums: null,
-    
-    /** 
+
+    /**
      * Clears any selections and, if necessary,
      * re-renders the parts of the chart that show selected marks.
-     * 
+     *
      * @type undefined
-     * @virtual 
+     * @virtual
      */
     clearSelections: function() {
         if(this.data.owner.clearSelected()) { this.updateSelections(); }
         return this;
     },
-    
+
     _updatingSelections: function(method, context) {
         this._suspendSelectionUpdate();
-        
-        try     { method.call(context || this);  } 
+
+        try     { method.call(context || this);  }
         finally { this._resumeSelectionUpdate(); }
     },
-    
+
     _suspendSelectionUpdate: function() {
-        if(this === this.root) { this._updateSelectionSuspendCount++; } 
+        if(this === this.root) { this._updateSelectionSuspendCount++; }
         else                   { this.root._suspendSelectionUpdate(); }
     },
-    
+
     _resumeSelectionUpdate: function() {
         if(this === this.root) {
             if(this._updateSelectionSuspendCount > 0) {
@@ -40,36 +40,39 @@ pvc.BaseChart
             this.root._resumeSelectionUpdate();
         }
     },
-    
-    /** 
+
+    /**
      * Re-renders the parts of the chart that show marks.
-     * 
+     *
      * @type undefined
-     * @virtual 
+     * @virtual
      */
     updateSelections: function(keyArgs) {
         if(this === this.root) {
             if(this._inUpdateSelections || this._updateSelectionSuspendCount) { return this; }
-            
+
             var selectedChangedDatumMap = this._calcSelectedChangedDatums();
             if(!selectedChangedDatumMap) { return this; }
-            
+
             pvc.removeTipsyLegends();
-            
+
             // Reentry control
             this._inUpdateSelections = true;
             try {
                 // Fire action
                 var action = this.options.selectionChangedAction;
                 if(action) {
+                    // Can change selection further...although it's probably
+                    // better to do that in userSelectionAction, called
+                    // before chosen datums' selected state is actually affected.
                     var selectedDatums = this.data.selectedDatums();
                     var selectedChangedDatums = selectedChangedDatumMap.values();
                     action.call(
-                        this.basePanel.context(), 
-                        selectedDatums, 
+                        this.basePanel.context(),
+                        selectedDatums,
                         selectedChangedDatums);
                 }
-                
+
                 // Rendering afterwards allows the action to change the selection in between
                 if(def.get(keyArgs, 'render', true)) {
                     this.useTextMeasureCache(function() { this.basePanel.renderInteractive(); }, this);
@@ -80,46 +83,46 @@ pvc.BaseChart
         } else {
             this.root.updateSelections();
         }
-        
+
         return this;
     },
-    
+
     _calcSelectedChangedDatums: function() {
         // Capture currently selected datums
         // Calculate the ones that changed.
-        
+
         // Caused by NoDataException ?
         if(!this.data) { return; }
-        
+
         var selectedChangedDatums;
         var nowSelectedDatums  = this.data.selectedDatumMap();
         var lastSelectedDatums = this._lastSelectedDatums;
         if(!lastSelectedDatums) {
             if(!nowSelectedDatums.count) { return; }
-            
+
             selectedChangedDatums = nowSelectedDatums.clone();
         } else {
             selectedChangedDatums = lastSelectedDatums.symmetricDifference(nowSelectedDatums);
-            
+
             if(!selectedChangedDatums.count) { return; }
         }
-        
+
         this._lastSelectedDatums = nowSelectedDatums;
 
         return selectedChangedDatums;
     },
-    
+
     _onUserSelection: function(datums) {
         if(!datums || !datums.length) { return datums; }
-        
+
         if(this === this.root) {
             // Fire action
             var action = this.options.userSelectionAction;
-            return action ? 
-                   (action.call(null, datums) || datums) :
+            return action ?
+                   (action.call(this.basePanel.context(), datums) || datums) :
                    datums;
         }
-        
+
         return this.root._onUserSelection(datums);
     }
 });
