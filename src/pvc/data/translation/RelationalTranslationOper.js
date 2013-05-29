23bf6e53bd876346b3daee5de5c4e609f5979273
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-05-29 23:47:55 +0100

    [FIX] (plot2SeriesIndex dataPart) Bug that occurred when an explicit reader for the "dataPart" dimension were defined, in a *relational* resultset. The option plot2SeriesIndexes was being registered even though the dataPart dimension was already being read.

--- src/pvc/data/translation/RelationalTranslationOper.js
+++ src/pvc/data/translation/RelationalTranslationOper.js
@@ -4,11 +4,11 @@
 
 /**
  * @name pvc.data.RelationalTranslationOper
- * 
- * @class Represents one translation operation, 
- * from a source matrix in relational format to 
+ *
+ * @class Represents one translation operation,
+ * from a source matrix in relational format to
  * an enumerable of atom arrays.
- * 
+ *
  * <p>
  * The default matrix-relational format is:
  * </p>
@@ -31,36 +31,36 @@
  * <p>
  * If the option <i>measuresIndexes</i> is specified,
  * additional value dimensions are created to receive the specified columns.
- * Note that these indexes may consume series and/or category indexes as well. 
+ * Note that these indexes may consume series and/or category indexes as well.
  * </p>
  * <p>
- * If only two metadata columns are provided, 
- * then a dummy 'series' column, with the constant null value, is added automatically. 
+ * If only two metadata columns are provided,
+ * then a dummy 'series' column, with the constant null value, is added automatically.
  * </p>
- * 
+ *
  * @extends pvc.data.MatrixTranslationOper
- *  
+ *
  * @constructor
  * @param {pvc.BaseChart} chart The associated chart.
  * @param {pvc.data.ComplexType} complexType The complex type that will represent the translated data.
  * @param {object} source The matrix-relational array to be translated.
  * The source is not modified.
  * @param {object} [metadata] A metadata object describing the source.
- * 
+ *
  * @param {object} [options] An object with translation options.
  * See additional available options in {@link pvc.data.MatrixTranslationOper}.
- * 
- * @param {(number|string)[]|number|string} [options.measuresIndexes] 
+ *
+ * @param {(number|string)[]|number|string} [options.measuresIndexes]
  * An array of indexes of columns of the source matrix
  * that contain value dimensions.
  * <p>
- * Multiple 'value' dimensions ('value', 'value2', 'value3', ...) 
+ * Multiple 'value' dimensions ('value', 'value2', 'value3', ...)
  * are bound in order to the specified indexes.
  * </p>
  * <p>
- * The option 'plot2DataSeriesIndexes' 
- * is incompatible with and 
- * takes precedence over 
+ * The option 'plot2DataSeriesIndexes'
+ * is incompatible with and
+ * takes precedence over
  * this one.
  * </p>
  * <p>
@@ -74,23 +74,23 @@ def
     M: 0, // number of measures
     C: 0, // number of categories
     S: 0, // number of series
-    
+
     _translType: "Relational",
-    
+
     _processMetadata: function() {
-        
+
         this.base();
-    
+
         var metadata = this.metadata;
-        
+
         var J = this.J; // metadata.length
-        
+
         // Split between series and categories
         var C = this.options.categoriesCount;
         if(C != null && (!isFinite(C) || C < 0)) { C = 0; }
 
         var S;
-        
+
         // Assuming duplicate valuesColIndexes is not valid
         // (v1 did not make this assumption)
         var valuesColIndexes, M;
@@ -98,7 +98,7 @@ def
             valuesColIndexes = pvc.parseDistinctIndexArray(this.options.measuresIndexes, 0, J - 1);
             M = valuesColIndexes ? valuesColIndexes.length : 0;
         }
-        
+
         var D; // discrete count = D = S + C
         if(M == null) {
             if(J > 0 && J <= 3 && (C == null || C === 1) && S == null) {
@@ -111,7 +111,7 @@ def
                 C = J >= 2 ? 1 : 0;
                 S = J >= 3 ? 1 : 0;
                 D = C + S;
-                
+
             } else if(C != null &&  C >= J) {
                 D = J;
                 C = J;
@@ -120,7 +120,7 @@ def
             } else {
                 // specified C wins over M, and by last S
                 var Mmax = C != null ? (J - C) : Infinity; // >= 1
-                
+
                 // colIndex has already been fixed on _processMetadata
                 // 0 = discrete
                 valuesColIndexes = def
@@ -134,7 +134,7 @@ def
                 M = valuesColIndexes.length;
             }
         }
-        
+
         if(D == null) {
             // M wins over C
             D = J - M;
@@ -154,7 +154,7 @@ def
                 C = D - S;
             }
         }
-        
+
         var seriesInRows = this.options.seriesInRows;
         var colGroupSpecs = [];
         if(D) {
@@ -162,11 +162,11 @@ def
             if(C                 ) { colGroupSpecs.push({name: 'C', count: C}); }
             if(S &&  seriesInRows) { colGroupSpecs.push({name: 'S', count: S}); }
         }
-        
+
         if(M) { colGroupSpecs.push({name: 'M', count: M}); }
-        
+
         var availableInputIndexes = def.range(0, J).array();
-        
+
         // If valuesColIndexes != null, these are reserved for values
         if(valuesColIndexes) {
             // Remove these indexes from available indexes
@@ -174,25 +174,25 @@ def
                 availableInputIndexes.splice(inputIndex, 1);
             });
         }
-        
+
         // Set the fields with actual number of columns of each group
         // Assign the input indexes of each group (Layout)
         var specsByName = {};
         colGroupSpecs.forEach(function(groupSpec) {
             var count = groupSpec.count;
             var name  = groupSpec.name;
-            
+
             // Index group by name
             specsByName[name] = groupSpec;
-            
-            if(valuesColIndexes && name === 'M') { groupSpec.indexes = valuesColIndexes; } 
+
+            if(valuesColIndexes && name === 'M') { groupSpec.indexes = valuesColIndexes; }
             else                                 { groupSpec.indexes = availableInputIndexes.splice(0, count); }
         });
-        
+
         this.M = M;
         this.S = S;
         this.C = C;
-        
+
         // Compose the total permutation array
         // that transforms the input into the virtual item "normal form":
         // S* C* M*
@@ -201,74 +201,76 @@ def
             var groupSpec = specsByName[name];
             if(groupSpec) { def.array.append(itemPerm, groupSpec.indexes); }
         });
-        
+
         this._itemInfos = itemPerm.map(this._buildItemInfoFromMetadata, this);
-        
+
         // The start indexes of each column group
         this._itemCrossGroupIndex = {S: 0, C: this.S, M: this.S + this.C};
-        
+
         this._itemPerm = itemPerm;
     },
-    
+
     logVItem: function() {
         return this._logVItem(['S', 'C', 'M'], {S: this.S, C: this.C, M: this.M});
     },
-    
-    /** 
-     * Default relational mapping from virtual item to dimensions. 
-     * @override 
+
+    /**
+     * Default relational mapping from virtual item to dimensions.
+     * @override
      */
     _configureTypeCore: function() {
         var me = this;
         var index = 0;
         var dimsReaders = [];
-        
+
         function add(dimGroupName, colGroupName, level, count) {
             var groupEndIndex = me._itemCrossGroupIndex[colGroupName] + count; // exclusive
             while(count > 0) {
                 var dimName = pvc.buildIndexedId(dimGroupName, level);
                 if(!me.complexTypeProj.isReadOrCalc(dimName)) { // Skip name if occupied and continue with next name
-                    
+
                     // use first available slot for auto dims readers as long as within the group slots
                     index = me._nextAvailableItemIndex(index);
                     if(index >= groupEndIndex) {
                         // this group has no more slots available
                         return;
                     }
-                    
+
                     dimsReaders.push({names: dimName, indexes: index});
-                    
+
                     index++; // consume index
                     count--;
                 }
-                
+
                 level++;
             }
         }
-        
+
         if(this.S > 0) { add('series',   'S', 0, this.S); }
         if(this.C > 0) { add('category', 'C', 0, this.C); }
         if(this.M > 0) { add('value',    'M', 0, this.M); }
 
         if(dimsReaders) { dimsReaders.forEach(this.defReader, this); }
-        
+
         // ----
         // The null test is required because plot2DataSeriesIndexes can be a number, a string...
-        var plot2DataSeriesIndexes = this.options.plot2DataSeriesIndexes;
-        if(plot2DataSeriesIndexes != null) {
-            var seriesReader = this._userDimsReadersByDim.series;
-            if(seriesReader) {
-                var dataPartDimName = this.options.dataPartDimName;
-                this._userRead(relTransl_dataPartGet.call(this, plot2DataSeriesIndexes, seriesReader), dataPartDimName);
+        var dataPartDimName = this.options.dataPartDimName;
+        if(dataPartDimName && !this.complexTypeProj.isReadOrCalc(dataPartDimName)) {
+            var plot2DataSeriesIndexes = this.options.plot2DataSeriesIndexes;
+            if(plot2DataSeriesIndexes != null) {
+                var seriesReader = this._userDimsReadersByDim.series;
+                if(seriesReader) {
+                    this._userRead(relTransl_dataPartGet.call(this, plot2DataSeriesIndexes, seriesReader), dataPartDimName);
+                }
             }
         }
     },
-    
+
     // Permutes the input rows
     _executeCore: function() {
         var dimsReaders = this._getDimensionsReaders();
         var permIndexes = this._itemPerm;
-        
+
         return def.query(this._getItems())
                   .select(function(item) {
                       item = pv.permute(item, permIndexes);
@@ -279,16 +281,16 @@ def
 
 /**
  * Obtains the dimension reader for dimension 'dataPart'.
- * 
+ *
  * @name pvc.data.RelationalTranslationOper#_dataPartGet
  * @function
- * @param {Array} plot2DataSeriesIndexes The indexes of series that are to be shown on the second axis. 
+ * @param {Array} plot2DataSeriesIndexes The indexes of series that are to be shown on the second axis.
  * @param {function} seriesReader Dimension series atom getter.
  * @type function
  */
 function relTransl_dataPartGet(plot2DataSeriesIndexes, seriesReader) {
     var me = this;
-    
+
     /* Defer calculation of plot2SeriesKeySet because *data* isn't yet available. */
     function calcAxis2SeriesKeySet() {
         var atoms = {};
@@ -299,7 +301,7 @@ function relTransl_dataPartGet(plot2DataSeriesIndexes, seriesReader) {
                                     if(value != null && value.v != null){
                                         value = value.v;
                                     }
-                                    
+
                                     return value || null;
                                 })
                                 /* distinct excludes null keys */
@@ -308,6 +310,6 @@ function relTransl_dataPartGet(plot2DataSeriesIndexes, seriesReader) {
 
         return me._createPlot2SeriesKeySet(plot2DataSeriesIndexes, seriesKeys);
     }
-    
+
     return this._dataPartGet(calcAxis2SeriesKeySet, seriesReader);
 }
