27c2fbfd90f03054004a93205a337817b71ba1ce
Duarte Cunha Leão <duarte.leao@gmail.com>
2013-10-24 02:54:03 +0100

    [CHG] Reverted "pen" headers to registering the CCC module as "cdf/lib/CCC/pvc-d1.0". Registering as version 2 would cause problems with Analyzer 5.0.

--- distPenToPlugins.sh
+++ distPenToPlugins.sh
@@ -13,7 +13,7 @@ echo "Make sure you have ran generatePvcBundle and generatePenBundle!"
 
 # CDF
 cp -f ${CCC_CDF_JS}def.js ${CDF_JS}
-cp -f ${CCC_CDF_JS}pvc-d2.0.js ${CDF_JS}
+cp -f ${CCC_CDF_JS}pvc-d1.0.js ${CDF_JS}
 cp -f ${CCC_CDF_JS}tipsy.* ${CDF_JS}
 cp -f ${CCC_CDF_JS}jquery.tipsy.js ${CDF_JS}
 cp -f ${CCC_CDF_JS}protovis.js ${CDF_JS}
