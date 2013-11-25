6f4f6ed42519477fd00545db8ddca8ce78b46acf
Pedro Vale <pedro.vale@webdetails.pt>
2013-11-25 11:31:59 +0000

    Changed karma conf to make it suitable for Ci builds

---
+++ karma.ci.conf.js
@@ -0,0 +1,94 @@
+// Karma configuration
+// Generated on Fri Nov 15 2013 00:09:22 GMT+0000 (GMT Standard Time)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['jasmine', 'requirejs'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'dist/test/cdf/jquery.js',
+      'test/test-main.js',
+      'test/test-utils.js',
+      'test/test-data-*.js',
+      'dist/test/cdf/**/*.js',
+      {pattern: 'test/**/*-spec.js', included: false}
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    preprocessors: {
+        "dist/test/cdf/lib/CCC/pvc-d1.0.js" : 'coverage'
+    },
+    
+    reporters: ['progress', 'coverage'],
+    
+       
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress', 'junit', 'html', 'coverage'],
+
+    coverageReporter: {
+        type : 'cobertura',
+        dir : 'dist/coverage/reports/'
+    },
+
+    junitReporter: {
+      outputFile: 'dist/test/test-results.xml',
+      suite: 'unit'    
+    },
+
+    // the default configuration
+    htmlReporter: {
+      outputDir:    'dist/test/karma_html',
+      templatePath: 'node_modules/karma-html-reporter/jasmine_template.html'
+    },
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['PhantomJS'],//, 'Firefox', 'IE', 'PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
