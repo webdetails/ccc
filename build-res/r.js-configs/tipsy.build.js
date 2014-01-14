da3f867435fcce7168b4bd7fd2da4fe878b52a9c
Diogo Mariano <diogo.mariano@webdetails.pt>
2014-01-14 14:12:01 +0000

    [ANALYZER-2248] - Reviewed ccc build process for generate(Def/Pen/Pvc)Bundle scripts through r.js Created ivy artifact for ccc. Assemble target overridden and calls builds for cdf (named ccc), pen (for pen.define) and amd (standard amd defines)

---
+++ build-res/r.js-configs/tipsy.build.js
@@ -0,0 +1,47 @@
+/*
+ * requirejs configuration file used to build the tipsy.js file
+ */
+
+({
+    appDir: "../../package-res/lib",
+    baseUrl: ".",
+    optimize: "uglify2",
+    dir: "../module-scripts",
+
+    //default wrap files, this is externally configured
+    wrap: {
+        startFile: "..",
+        endFile: ".."
+    },
+
+    uglify2: {
+        output: {
+            beautify: true,
+            max_line_len: 1000
+        },
+        compress: {
+            sequences: false,
+            global_defs: {
+                DEBUG: false
+            }
+        },
+        warnings: true,
+        mangle: false
+    },
+
+    removeCombined: true,
+
+    preserveLicenseComments: true,
+
+    modules: [
+        {
+            name: "tipsy",
+            create: false
+        }
+    ],
+
+    skipModuleInsertion: true,
+
+    skipDirOptimize: true,
+    optimizeCss: "none"
+})
