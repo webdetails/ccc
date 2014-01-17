4e3e359b71c9da3bbe68411b7270913785b242bb
e-cuellar <ecuellar@pentaho.com>
2014-01-17 00:19:51 -0500

    [ANALYZER-2259] Added option for controlling the handling of null categories "empty slices"

--- package-res/pvc/visual/plot/SunburstPlot.js
+++ package-res/pvc/visual/plot/SunburstPlot.js
@@ -86,5 +86,11 @@ pvc.visual.SunburstPlot.optionsDef = def.create(
           resolve: '_resolveFull',
           cast: pvc.parseSunburstSliceOrder,
           value: 'bySizeDescending'
+        },
+        
+        EmptySlicesVisible: {
+          resolve: '_resolveFull',
+          cast: Boolean,
+          value: false
         }
     });
