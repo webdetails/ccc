d43aa55ec581098060c7dc8117798537f6b820d6
Duarte Cunha Leão <duarte.leao@gmail.com>
2014-03-31 17:08:30 +0100

    [CCC-119] - IE 8 compatibility with some chart types. * Fixes Parallel Coordinates in IE8.

--- package-res/pvc/pvc.js
+++ package-res/pvc/pvc.js
@@ -2,6 +2,22 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+if(!Array.prototype.every) Array.prototype.every = function(fun /*, thisArg */) {
+    'use strict';
+
+    if(this == null) throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if(typeof fun !== 'function') throw new TypeError();
+
+    var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+    for(var i = 0; i < len; i++) {
+        if(i in t && !fun.call(thisArg, t[i], i, t)) return false;
+    }
+    return true;
+};
+
 /*global pvc:true */
 var pvc = def.globalSpace('pvc', {
     // 0 - off
