9fbf9c92c90510186a2d6589e2eb80f8862e4b15
Duarte Cunha Leão <duarte.leao@gmail.com>
2014-10-20 18:46:44 +0100

    [CLEANUP] - Changed CCC site charts folder in sync script.

--- syncToDemoDotWebdetails.sh
+++ syncToDemoDotWebdetails.sh
@@ -4,4 +4,4 @@ ant dist
 ./generateJsDocs.sh
 ./generateSite.sh
 
-rsync -rovz dist/site/ctools/charts/ demo.webdetails.org:/var/www/www.webdetails.pt/htdocs/ctools/charts/
+rsync -rovz dist/site/ctools/ccc/charts/ demo.webdetails.org:/var/www/www.webdetails.pt/htdocs/ctools/ccc/charts/
