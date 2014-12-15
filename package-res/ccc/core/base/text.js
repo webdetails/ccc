27b26a7b25941d1062bfd1406c9e8066b2db87b6
Duarte Cunha Leão <duarte.leao@gmail.com>
2014-12-15 16:40:31 +0000

    [CDF-379] - CCC - A very small axisSize sometimes leads to an infinite loop * Now, really fixed it.

--- package-res/ccc/core/base/text.js
+++ package-res/ccc/core/base/text.js
@@ -5,7 +5,7 @@
 pvc.text = {
     getFitInfo: function(w, h, text, font, diagMargin) {
         if(text === '') return {h: true, v: true, d: true};
-        
+
         var len = pv.Text.measureWidth(text, font);
         return {
             h: len <= w,
@@ -19,19 +19,19 @@ pvc.text = {
             clipLen   = 3/2 * terminLen;
         return pvc.text.trimToWidth(len, text, font, trimTerminator, before, clipLen);
     },
-    
+
     trimToWidth: function(len, text, font, trimTerminator, before, clipLen) {
         if(text === '') return text;
-  
+
         var textLen = pv.Text.measureWidth(text, font);
         if(textLen <= len) return text;
-        
+
         // ----------------
         // Trim needed
         if(textLen > len * 1.5) // threshold for using other algorithm
             return pvc.text.trimToWidthBin(len, text, font, trimTerminator, before, clipLen);
-        
-        len -= Math.max(0, pv.Text.measureWidth(trimTerminator, font));
+
+        len = Math.max(0, len - pv.Text.measureWidth(trimTerminator, font));
 
         while(textLen > len) {
             text = before ? text.slice(1) : text.slice(0, text.length -1);
@@ -45,10 +45,10 @@ pvc.text = {
 
         return before ? (trimTerminator + text) : (text + trimTerminator);
     },
-    
+
     trimToWidthBin: function(len, text, font, trimTerminator, before, clipLen) {
 
-        len -= Math.max(0, pv.Text.measureWidth(trimTerminator, font));
+        len = Math.max(0, len - pv.Text.measureWidth(trimTerminator, font));
 
         var ilen = text.length,
             high = ilen - 2,
@@ -59,7 +59,7 @@ pvc.text = {
         while(low <= high && high > 0) {
 
             mid = Math.ceil((low + high)/2);
-            
+
             var textMid = before ? text.slice(ilen - mid) : text.slice(0, mid);
             textLen = pv.Text.measureWidth(textMid, font);
             if(textLen > len) {
@@ -77,15 +77,15 @@ pvc.text = {
         if(clipLen && textLen <= clipLen) return "";
         return before ? (trimTerminator + text) : (text + trimTerminator);
     },
-    
+
     justify: function(text, lineWidth, font) {
         var lines = [];
 
         // Not even one letter fits...
         if(lineWidth < pv.Text.measureWidth('a', font)) return lines;
-        
+
         var words = (text || '').split(/\s+/);
-        
+
         var line = "";
         while(words.length) {
             var word = words.shift();
@@ -93,33 +93,33 @@ pvc.text = {
                 var nextLine = line ? (line + " " + word) : word;
                 if(pv.Text.measureWidth(nextLine, font) > lineWidth) {
                     // The word by itself may overflow the line width
-                    
+
                     // Start new line
                     if(line) lines.push(line);
-                    
+
                     line = word;
                 } else {
-                    line = nextLine; 
+                    line = nextLine;
                 }
             }
         }
-        
+
         if(line) lines.push(line);
-        
+
         return lines;
     },
-    
+
     /* Returns a label's BBox relative to its anchor point */
     getLabelBBox: function(textWidth, textHeight, align, baseline, angle, margin) {
-            
+
         var polygon = pv.Label.getPolygon(textWidth, textHeight, align, baseline, angle, margin);
-        
+
         var bbox             = polygon.bbox();
         bbox.source          = polygon;
         bbox.sourceAngle     = angle;
         bbox.sourceAlign     = align;
         bbox.sourceTextWidth = textWidth;
-        
+
         return bbox;
     }
 };
