2295a6594e8f418525f9e77ce78252de62cf844c
David Santos <david.santos@webdetails.pt>
2015-01-08 16:09:07 +0000

    [CDF-422]: CCC - Tooltip moves when hovering over a chart dot

--- package-res/ccc/plugin/scatter/scatter-plot-panel.js
+++ package-res/ccc/plugin/scatter/scatter-plot-panel.js
@@ -351,7 +351,13 @@ def
         var dot = new pvc.visual.DotSizeColor(me, me.pvLine, {
                 extensionId: 'dot',
                 wrapper:     wrapper,
-                activeSeriesAware: me.linesVisible
+                activeSeriesAware: me.linesVisible,
+                tooltipArgs: {
+                    options: {
+                        ignoreRadius: rootScene.isSizeBound ? false : me.linesVisible
+                    }
+                }
+
             })
             .override('x',  function(scene) { return scene.basePosition;  })
             .override('y',  function(scene) { return scene.orthoPosition; })
