f84ba3e137511bac053c0e6633bfb0e4a5b93148
Duarte Cunha Leão <duarte.leao@gmail.com>
2017-09-27 10:57:00 +0100

    Fixing require.config.js file.

--- charts/lib/amd/require.config.js
+++ charts/lib/amd/require.config.js
@@ -4,9 +4,9 @@
 
 require.config({
     paths: {
-        "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js",
-        "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min.js",
-        "css": "//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css.min.js",
+        "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min",
+        "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
+        "css": "//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css.min",
 
         "ccc": "//webdetails.github.io/ccc/charts/lib/amd"
     }
