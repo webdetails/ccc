12cc43cfecf115b7b8a9f5825a2ca49114994537
Duarte Cunha Leão <duarte.leao@gmail.com>
2017-09-27 10:39:15 +0100

    [CLEANUP] Updating site sources to match gh-pages branch.

---
+++ site/root/charts/lib/amd/require.config.js
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+require.config({
+    paths: {
+        "jquery": "//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min",
+        "text": "//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min",
+        "css": "//cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css.min",
+
+        "ccc": "//webdetails.github.io/ccc/charts/lib/amd"
+    }
+});
