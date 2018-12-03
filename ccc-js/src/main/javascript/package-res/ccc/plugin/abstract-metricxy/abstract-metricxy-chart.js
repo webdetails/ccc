8e97bdf2643b66d0039149226f933af42f8edbe9
Duarte Cunha Leão <duarte.leao@gmail.com>
2018-12-03 16:29:24 +0000

    [SP-4754] PAZ: In Scatter chart, adding a level to Multi-Chart drop zone results in a different order

--- ccc-js/src/main/javascript/package-res/ccc/plugin/abstract-metricxy/abstract-metricxy-chart.js
+++ ccc-js/src/main/javascript/package-res/ccc/plugin/abstract-metricxy/abstract-metricxy-chart.js
@@ -11,5 +11,25 @@ def
 .type('pvc.MetricXYAbstract', pvc.CartesianAbstract)
 .add({
     // Has no representation in this chart's main plot type.
-    _defaultAxisBandSizeRatio: 1
+    _defaultAxisBandSizeRatio: 1,
+
+    _getIsNullDatum: function() {
+        var me = this, measureDimNames, M;
+
+        // If x or y are null, it is a null datum.
+        return function(datum) {
+            if(!measureDimNames) {
+                measureDimNames = def.query([me.visualRoles.x, me.visualRoles.y])
+                    .selectMany(function(role) { return role.grouping.dimensionNames(); })
+                    .distinct()
+                    .array();
+                M = measureDimNames.length;
+            }
+
+            if(!M) return false;
+            var atoms = datum.atoms;
+            for(var i = 0 ; i < M ; i++) if(atoms[measureDimNames[i]].value == null) return true;
+            return false;
+        };
+    }
 });
