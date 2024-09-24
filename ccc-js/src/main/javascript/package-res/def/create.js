10e38bc72dc206bcfed11443dd240deaa88daf18
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 16:32:40 -0400

    HNC-766: updated license and copyright headers

--- ccc-js/src/main/javascript/package-res/def/create.js
+++ ccc-js/src/main/javascript/package-res/def/create.js
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 // Creates an object whose prototype is the specified object.
 def.create = function(/*[deep,] baseProto, mixin1, mixin2, ...*/) {
     var mixins = A_slice.call(arguments),
