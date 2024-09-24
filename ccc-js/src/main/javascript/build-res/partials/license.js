a578664f2024b2a38fd5cd941ecc685472e6a545
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:24:01 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 10e38bc72dc206bcfed11443dd240deaa88daf18.

--- ccc-js/src/main/javascript/build-res/partials/license.js
+++ ccc-js/src/main/javascript/build-res/partials/license.js
@@ -1,12 +1,13 @@
-/*! ******************************************************************************
+/*!
+ * Copyright 2002 - 2018 Webdetails, a Hitachi Vantara company.  All rights reserved.
  *
- * Pentaho
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
  *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
 
--- ccc-js/src/main/javascript/package-res/lib/protovis-compat.js
+++ ccc-js/src/main/javascript/package-res/lib/protovis-compat.js
@@ -1,14 +1,15 @@
-/*! ******************************************************************************
+/*!
+ * Copyright 2002 - 2017 Webdetails, a Hitachi Vantara company.  All rights reserved.
+ * 
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
  *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
 
 var protovisId = "./protovis";
 var protovisMsieId = "./protovis-msie";
